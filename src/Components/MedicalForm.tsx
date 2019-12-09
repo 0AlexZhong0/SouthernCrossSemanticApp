65be5809ffe65c12a4feb5ae90820af9b209a9f3
Daphne <dsmhan7@gmail.com>
2019-12-09 18:54:37 +1300

    adding second issue by separating+dupicating some functions

--- src/Components/MedicalForm.tsx
+++ src/Components/MedicalForm.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import CustomButton from "Components/Helpers/CustomButton";
 import CustomCheckBox from "Components/Helpers/CustomCheckBox";
-import { diagnoseConditionsFromSymptoms, getIssueId, getIssueInfo } from "SymptomCheckerApi/mainApi"
+import { diagnoseConditionsFromSymptoms1,diagnoseConditionsFromSymptoms2, getIssueId1, getIssueId2, getIssueInfo } from "SymptomCheckerApi/mainApi"
 
 // include all these code in a separate file called MedicalForm.tsx
 interface IForm {
@@ -51,12 +51,32 @@ const MedicalForm = (): JSX.Element => {
   //   })
   // }
 
-  const populateSymptoms = (condition: string): void => {
-    const issueId: number = getIssueId(condition)
-    const symptomsCheckBoxes: JSX.Element[] = []
 
+
+  // D: seperate from old "populateSymptoms" function into two functions to make hard code easier
+  
+  // D: Don't know why but presssing "get symptoms" will return the same conditions depending
+  // on what issue is selected, but this function returns the correct hard code conditions
+  // need to keep working on this hard code
+  
+  
+  const populateSymptomsID1 = (condition: string): void => {
+    const issueId: number = getIssueId1(condition)
+    populateSymptoms(issueId)
+  }
+
+  const populateSymptomsID2 = (condition: string): void => {
+    const issueId: number = getIssueId2(condition)
+    populateSymptoms(issueId)
+  }
+
+
+const populateSymptoms = (issueId: number): void => {
+    const symptomsCheckBoxes: JSX.Element[] = []
     getIssueInfo(issueId).then((res: { PossibleSymptoms: string }) => {
+      
       const possibleSyms: string[] = res.PossibleSymptoms.split(",")
+      console.log(possibleSyms)
       possibleSyms.forEach((symptom: string, index: number): void => {
         symptomsCheckBoxes.push(<CustomCheckBox text={symptom} key={symptom + `${index}`} />)
       })
@@ -65,9 +85,24 @@ const MedicalForm = (): JSX.Element => {
     })
   }
 
-  const populateConditions = (): void => {
+  
+
+  const populateConditions1 = (): void => {
     const conditions: JSX.Element[] = []    
-    const diagnoseResult: Promise<any> = diagnoseConditionsFromSymptoms(form.symptomsArray, "male", 1993)
+    const diagnoseResult: Promise<any> = diagnoseConditionsFromSymptoms1(form.symptomsArray, "male", 1993)
+    // populate the potential related issues and display more checkbox onto the page            
+    diagnoseResult.then((result: IResult[]): void => {
+      result.forEach((issue: IIssue, index: number): void => {
+        const IssueName: string = issue.Issue.Name
+        conditions.push(<CustomCheckBox text={IssueName} key={IssueName + `${index}`} />)
+      })
+    setForm({ conditions } as IForm)
+    })
+  }
+
+  const populateConditions2 = (): void => {
+    const conditions: JSX.Element[] = []    
+    const diagnoseResult: Promise<any> = diagnoseConditionsFromSymptoms2(form.symptomsArray, "male", 1993)
     // populate the potential related issues and display more checkbox onto the page            
     diagnoseResult.then((result: IResult[]): void => {
       result.forEach((issue: IIssue, index: number): void => {
@@ -82,13 +117,24 @@ const MedicalForm = (): JSX.Element => {
     <React.Fragment>
       <CustomCheckBox text="Heart Attack" />
       <div>
-      <CustomButton loadComponent={() => populateSymptoms("Heart Attack")} title="Get Symptoms"/>
+      <CustomButton loadComponent={() => populateSymptomsID1("Heart Attack")} title="Get Symptoms"/>
       </div>
           <div>
               {form.symptomsCheckBoxes}
-              <CustomButton loadComponent={populateConditions} title="Get Conditions" />
+              <CustomButton loadComponent={populateConditions1} title="Get Conditions" />
               {form.conditions}
           </div>
+
+     <CustomCheckBox text="Abdominal hernia" />
+      <div>
+      <CustomButton loadComponent={() => populateSymptomsID2("Abdominal hernia")} title="Get Symptoms"/>
+      </div>
+          <div>
+              {form.symptomsCheckBoxes}
+              <CustomButton loadComponent={populateConditions2} title="Get Conditions" />
+              {form.conditions}
+          </div>
+
     </React.Fragment>
   );
 };
--- src/SymptomCheckerApi/mainApi.tsx
+++ src/SymptomCheckerApi/mainApi.tsx
@@ -7,15 +7,24 @@ import requests from "SymptomCheckerApi/ApiUtilities/requests";
 // hard-coded functions session, perhaps do not need to export all these functions
 // think of a way to encompass all the functions written below
 // 87 is a heart attack example
-export const getIssueId = (issue: string): number => {
+export const getIssueId1 = (issue: string): number => {
   return 87
 }
 
-export const getSymptomIds = (symptoms: string[]): number[] => {
+export const getIssueId2 = (issue: string): number => {
+  return 130
+}
+
+
+export const getSymptomIds1 = (symptoms: string[]): number[] => {
   const symptomIds: number[] = [29, 144, 17]  // select the top three of 
   return symptomIds
 }
 
+export const getSymptomIds2 = (symptoms: string[]): number[] => {
+  const symptomIds: number[] = [44, 10]  // only has two symptoms
+  return symptomIds
+}
 
 // token required session
 export const getIssueInfo = (issuedId: number): Promise<any> => {
@@ -26,14 +35,14 @@ export const getIssueInfo = (issuedId: number): Promise<any> => {
         `/issues/${issuedId}/info?token=${token.Token}&format=json&language=en-gb`;
       const issueInfoResult = requests(issueInfoUri);
       return issueInfoResult;
+      
     })
     .catch(err => console.log(err));
-
   return issueInfo;
 };
 
-export const diagnoseConditionsFromSymptoms = (symptoms: string[], sex: string, year_of_birth: number | string): Promise<any> =>  {
-  const symptomIds = getSymptomIds(symptoms)
+export const diagnoseConditionsFromSymptoms1 = (symptoms: string[], sex: string, year_of_birth: number | string): Promise<any> =>  {
+  const symptomIds = getSymptomIds1(symptoms)
   const diagnosis = getAccessToken().then((token: {Token: string}) => {
     const diagnoseUri: string = HEALTH_BASE_URL +  `/diagnosis?symptoms=[${symptomIds}]&gender=${sex}&year_of_birth=${year_of_birth}&token=${token.Token}&format=json&language=en-gb`
     const relatedSymptoms = requests(diagnoseUri)
@@ -42,4 +51,16 @@ export const diagnoseConditionsFromSymptoms = (symptoms: string[], sex: string, 
   })
    
   return diagnosis
-}
+};
+
+export const diagnoseConditionsFromSymptoms2 = (symptoms: string[], sex: string, year_of_birth: number | string): Promise<any> =>  {
+  const symptomIds = getSymptomIds2(symptoms)
+  const diagnosis = getAccessToken().then((token: {Token: string}) => {
+    const diagnoseUri: string = HEALTH_BASE_URL +  `/diagnosis?symptoms=[${symptomIds}]&gender=${sex}&year_of_birth=${year_of_birth}&token=${token.Token}&format=json&language=en-gb`
+    const relatedSymptoms = requests(diagnoseUri)
+    
+    return relatedSymptoms
+  })
+   
+  return diagnosis
+};
