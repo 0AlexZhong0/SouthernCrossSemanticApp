2f40c84d65097131ec82a5241b975033d1f8c9f0
Daphne <dsmhan7@gmail.com>
2019-12-16 16:20:46 +1300

    passing user birth year into diagnoseconditions function dynamically

--- client/src/Components/Helpers/CustomCheckBox.tsx
+++ client/src/Components/Helpers/CustomCheckBox.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
 import { Checkbox, FormControlLabel } from "@material-ui/core";
 
-const CustomCheckBox = (props: { text: string }): JSX.Element => {
+const CustomCheckBox = (props: { text: string, value:any }): JSX.Element => {
   // also change the state so that the app knows what are the conditions that got populated
   const [isChecked, setIsChecked] = React.useState(false);
+  const [selectedItem, setSelectedItem] = React.useState('');
+
   const tickOrUntick = (): void => {
     if (isChecked) {
       setIsChecked(false);
     } else {
       setIsChecked(true);
+      setSelectedItem(props.text);
+     
     }
   };
+  
+
 
   return (
     // Populate checkboxes with all the possible conditions
--- client/src/Components/MedicalForm.tsx
+++ client/src/Components/MedicalForm.tsx
@@ -4,9 +4,6 @@ import CardContent from "@material-ui/core/CardContent";
 import CustomButton from "Components/Helpers/CustomButton";
 import CustomCheckBox from "Components/Helpers/CustomCheckBox";
 import { diagnoseConditionsFromSymptoms, getIssueId, getIssueInfo } from "SymptomCheckerApi/mainApi"
-import FormControl from "@material-ui/core/FormControl";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 
 interface IIssue {
@@ -31,7 +28,7 @@ const MedicalForm = (): JSX.Element => {
 
       const possibleSyms: string[] = res.PossibleSymptoms.split(",")
       possibleSyms.forEach((symptom: string, index: number): void => {
-        symptomsCheckBoxes.push(<CustomCheckBox text={symptom} key={symptom + `${index}`} />)
+        symptomsCheckBoxes.push(<CustomCheckBox text={symptom} key={symptom + `${index}`} value = ""/>)
       })
       setSymptomsCheckBoxes(symptomsCheckBoxes)
       setsymptomsArray(possibleSyms)
@@ -40,21 +37,50 @@ const MedicalForm = (): JSX.Element => {
 
   }
 
-  const populateConditions = (): void => {
+ const populateConditions = (): void => {
     const conditions: JSX.Element[] = []
-    const diagnoseResult: Promise<any> = diagnoseConditionsFromSymptoms(symptomsArray, "male", 1993)
+    console.log({gender});
+    console.log(year);
+    const diagnoseResult: Promise<any> = diagnoseConditionsFromSymptoms(symptomsArray, "male", year)
     // populate the potential related issues and display more checkbox onto the page            
     diagnoseResult.then((result: IResult[]): void => {
       result.forEach((issue: IIssue, index: number): void => {
         const IssueName: string = issue.Issue.Name
-        conditions.push(<CustomCheckBox text={IssueName} key={IssueName + `${index}`} />)
+        conditions.push(<CustomCheckBox text={IssueName} key={IssueName + `${index}`} value = ""/>)
       })
       setConditionsCheckBoxes(conditions)
       setSymptomsCheckBoxes([])
     })
   }
 
-  return (
+  // D: changed select drop down to text field as cumbersome with 6 drop downs but can revert later 
+  //    need to implement error mechanisms later if time permits
+ 
+  const [date, setDate] = React.useState('');  
+  const [month, setMonth] = React.useState('');  
+  const [year, setYear] = React.useState('');
+
+  const [gender, setGender] = React.useState('');
+
+  const handledateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(event.target.value);
+  };
+
+  const handlemonthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMonth(event.target.value);
+  };
+
+  // Need year for diagnoseConditionsFromSymptoms function
+  const handleyearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setYear(event.target.value);
+  };
+
+
+  console.log("parent",{gender})
+
+ 
+  
+return (
     <React.Fragment>
          <Card className = "userdetails">
           <CardContent>
@@ -71,117 +97,32 @@ const MedicalForm = (): JSX.Element => {
              </div>
 
              <br></br>
+            
+            <div>
+            DOB {" "} 
+            
+            <TextField id="outlined-basic" label="d" variant="outlined" onChange = {handledateChange} value = {date} />
+            {" "} 
+            <TextField id="outlined-basic" label="m" variant="outlined" onChange = {handlemonthChange} value = {month}/>
+            {" "} 
+            <TextField id="outlined-basic" label="y" variant="outlined" onChange = {handleyearChange} value = {year}/>
 
-             <div>
-                 DOB {" "}  
-                <FormControl>
-                    <Select>
-                        <MenuItem value={0}>0</MenuItem>
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        </Select>
-                </FormControl>
-                {" "}
-                <FormControl>
-                    <Select>
-                        <MenuItem value={0}>0</MenuItem>
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        <MenuItem value={3}>3</MenuItem>
-                        <MenuItem value={4}>4</MenuItem>
-                        <MenuItem value={5}>5</MenuItem>
-                        <MenuItem value={6}>6</MenuItem>
-                        <MenuItem value={7}>7</MenuItem>
-                        <MenuItem value={8}>8</MenuItem>
-                        <MenuItem value={9}>9</MenuItem>
-                        </Select>
-                </FormControl>
-                 {" "}
-                 dd
-                 {" "}
-                 <FormControl>
-                    <Select>
-                        <MenuItem value={0}>1</MenuItem>
-                        <MenuItem value={1}>2</MenuItem>
-                        <MenuItem value={2}>3</MenuItem>
-                        <MenuItem value={3}>4</MenuItem>
-                        <MenuItem value={4}>5</MenuItem>
-                        <MenuItem value={5}>6</MenuItem>
-                        <MenuItem value={6}>7</MenuItem>
-                        <MenuItem value={7}>8</MenuItem>
-                        <MenuItem value={8}>9</MenuItem>
-                        <MenuItem value={9}>10</MenuItem>
-                        <MenuItem value={10}>11</MenuItem>
-                        <MenuItem value={11}>12</MenuItem>
-                        </Select>
-                </FormControl>
-                 {" "}
-                 mm
-                 {" "}
-                 <FormControl>
-                    <Select>
-                        <MenuItem value={0}>1</MenuItem>
-                        <MenuItem value={1}>2</MenuItem>
-                    </Select>
-                </FormControl>
-                {" "}
-                <FormControl>
-                    <Select>
-                        <MenuItem value={0}>1</MenuItem>
-                        <MenuItem value={1}>2</MenuItem>
-                        <MenuItem value={2}>3</MenuItem>
-                        <MenuItem value={3}>4</MenuItem>
-                        <MenuItem value={4}>5</MenuItem>
-                        <MenuItem value={5}>6</MenuItem>
-                        <MenuItem value={6}>7</MenuItem>
-                        <MenuItem value={7}>8</MenuItem>
-                        <MenuItem value={8}>9</MenuItem>
-                        </Select>
-                </FormControl>
-                {" "}
-                <FormControl>
-                    <Select>
-                        <MenuItem value={0}>1</MenuItem>
-                        <MenuItem value={1}>2</MenuItem>
-                        <MenuItem value={2}>3</MenuItem>
-                        <MenuItem value={3}>4</MenuItem>
-                        <MenuItem value={4}>5</MenuItem>
-                        <MenuItem value={5}>6</MenuItem>
-                        <MenuItem value={6}>7</MenuItem>
-                        <MenuItem value={7}>8</MenuItem>
-                        <MenuItem value={8}>9</MenuItem>
-                        </Select>
-                </FormControl>
-                {" "}
-                <FormControl>
-                    <Select>
-                        <MenuItem value={0}>1</MenuItem>
-                        <MenuItem value={1}>2</MenuItem>
-                        <MenuItem value={2}>3</MenuItem>
-                        <MenuItem value={3}>4</MenuItem>
-                        <MenuItem value={4}>5</MenuItem>
-                        <MenuItem value={5}>6</MenuItem>
-                        <MenuItem value={6}>7</MenuItem>
-                        <MenuItem value={7}>8</MenuItem>
-                        <MenuItem value={8}>9</MenuItem>
-                        </Select>
-                </FormControl>
-                 {" "}
-             </div>
+            </div>
              
              <br></br>
 
              <div>
                  Biological sex
                  {" "}
-                 <CustomCheckBox text="Male" />
-                 <CustomCheckBox text="Female" />
+                 <CustomCheckBox text="Male" value = {gender}/>
+                 <CustomCheckBox text="Female" value = {setGender}/>
+                 
              </div>
 
         </CardContent>
       </Card>
       <div>
-        <CustomCheckBox text="Heart Attack" />
+        <CustomCheckBox text="Heart Attack" value = ""/>
         <div>
           <CustomButton loadComponent={() => populateSymptoms("Heart Attack")} title="Get Symptoms" />
           {symptomsCheckBoxes}
@@ -195,7 +136,7 @@ const MedicalForm = (): JSX.Element => {
       <br />
 
       <div>
-        <CustomCheckBox text="Obstruction of a pulmonary artery" />
+        <CustomCheckBox text="Obstruction of a pulmonary artery" value = ""/>
         <div>
           <CustomButton loadComponent={() => populateSymptoms("Obstruction of a pulmonary artery")} title="Get Symptoms" />
           <br />
@@ -208,7 +149,7 @@ const MedicalForm = (): JSX.Element => {
       <br />
 
       <div>
-        <CustomCheckBox text="Coronary heart disease" />
+        <CustomCheckBox text="Coronary heart disease" value = ""/>
         <div>
           <CustomButton loadComponent={() => populateSymptoms("Coronary heart disease")} title="Get Symptoms" />
           <br />
